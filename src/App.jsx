c44e355160301f10b539602e3057056089d9b115
nirman2004 <nd2719@ece.jgec.ac.in>
2024-11-20 01:47:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Highlights from './components/Highlights'
+import Hero from './components/Hero'
+import Model from './components/Model'
+import Features from './components/Features'
+import Chip from './components/Chip'
+
+const App = () => {
+  return (
+    
+    <main className='bg-black'>
+<Navbar/>
+<Hero/>
+<Highlights/>
+<Model/>
+<Features/>
+<Chip/>
+    </main>  
+
+
+        
+      )
+    }
+  
+
+
+export default App
